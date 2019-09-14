be18103af6314e1f6081a81763dd5183cb55fbfe
NSAdeveloperFN <55312872+NSAdeveloperFN@users.noreply.github.com>
2019-09-14 19:27:08 +1000

    Do smth

--- main.cpp
+++ main.cpp
@@ -5,7 +5,53 @@
 
 using namespace std;
 
+class munit
+{
+public:
+	int ret() { return num; }
+	munit(int unit)
+	{
+		num = unit;
+		pnum = unit;
+	}
+	void make_n();
+private:
+	int num;
+	int pnum;
+};
+
+class matrix;
+
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Hello User!\n"
+	 << "input yo matrix unit: ";
+	int n;
+	cin >> n;
+	munit u(n);
+	cout << "\nYep ur matrix unit is " << u.ret() << endl;
+	cout << "\nLet's get started!" << endl;
+
+	char o = 0;
+	while (o != 'q')
+	{
+		cout << endl << "q to quit\nn to negative" << endl << "Operation: ";
+		cin >> o;
+		switch (o)
+		{
+		case 'n':
+			u.make_n();
+			break;
+		default:
+			break;
+		}
+	}
+	cout << "\nYep ur matrix unit is " << u.ret() << endl;
+	return 0;
+}
+
+void munit::make_n()
+{
+	pnum = num;
+	num = 0-num;
 }
